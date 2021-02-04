983e6fb66595c0a4364fb132d8dbc34bc5ff3add
Lukman9812 <76692029+Lukman9812@users.noreply.github.com>
2021-02-04 18:08:22 +0100

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Main.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public static class main
+    {
+        static void Main(string[] args)
+        {
+            Zdravic zdravic = new Zdravic("Ahoj uživatelu", "Karel", "XYZ");
+            zdravic.Pozdrav();
+            zdravic.SetJmeno("Lukas");
+            zdravic.Pozdrav();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Zdravic.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Zdravic
+    {
+        private string _text;
+        private string _dodatek;
+        private string _jmeno;
+        // takto se běžně tvoří soukromý string
+
+        public Zdravic()
+        {
+
+        }
+
+        public Zdravic(string text, string jmeno, string dodatek)
+        {
+            _text = text;
+            _dodatek = dodatek;
+            _jmeno = jmeno;
+        }
+
+        public void SetText(string text)
+        {
+            _text = text;
+        }
+
+        public string GetText()
+        {
+            return _text;
+        }
+
+        public void SetJmeno(string jmeno)
+        {
+            _jmeno = jmeno;
+        }
+
+        public string GetJmeno()
+        {
+            return _jmeno;
+        }
+
+        public void SetDodatek(string dodatek)
+        {
+            _dodatek = dodatek;
+        }
+
+        public string GetDodatek()
+        {
+            return _dodatek;
+        }
+
+        public void Pozdrav()   // Pozdrav je metoda
+
+        {
+            Console.WriteLine("{0} {1} {2}", _text, _jmeno, _dodatek);      // dané čísla určují pořadí proměnných za ním (1. text pak 2. jmeno
+
+        }
+    }
+}
